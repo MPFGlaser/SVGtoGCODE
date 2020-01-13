be80bde8412fd5661fa060f956d02950b18fd4ae
Max Glaser <mpf.glaser@gmail.com>
2020-01-13 13:51:42 +0100

    Added parameters for workspace configuration

--- SVGtoGCODE/Workspace.cs
+++ SVGtoGCODE/Workspace.cs
@@ -8,10 +8,31 @@ namespace SVGtoGCODE
 {
     public class Workspace
     {
+        private int _sizeX;
+        private int _sizeY;
+        private int _offsetX;
+        private int _offsetY;
+        private int _printHeight;
+        private int _moveHeight;
+        private int _printSpeed;
+        private int _moveSpeed;
+
         // Holds the information about the defined workspace, such as the height and width dimensions.
         public Workspace()
         {
 
+        }
+
+        public Workspace(int sizeX, int sizeY, int offsetX, int offsetY, int printHeight, int moveHeight, int printSpeed, int moveSpeed)
+        {
+            _sizeX = sizeX;
+            _sizeY = sizeY;
+            _offsetX = offsetX;
+            _offsetY = offsetY;
+            _printHeight = printHeight;
+            _moveHeight = moveHeight;
+            _printSpeed = printSpeed;
+            _moveSpeed = moveSpeed;
         }
     }
 }
