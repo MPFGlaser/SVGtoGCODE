6bc0dfe6225b9a0828bb3e4e75790e23fcdfd8c9
Max Glaser <mpf.glaser@gmail.com>
2020-01-14 11:25:18 +0100

    Added editing & saving of settings Added default values to setting boxes on creation

--- SVGtoGCODE/ConversionSettings.xaml.cs
+++ SVGtoGCODE/ConversionSettings.xaml.cs
@@ -23,6 +23,14 @@ namespace SVGtoGCODE
         public ConversionSettings()
         {
             InitializeComponent();
+            SettingBoxSizeX.Text = Properties.Settings.Default.SizeX.ToString();
+            SettingBoxSizeY.Text = Properties.Settings.Default.SizeY.ToString();
+            SettingBoxOffsetX.Text = Properties.Settings.Default.OffsetX.ToString();
+            SettingBoxOffsetY.Text = Properties.Settings.Default.OffsetY.ToString();
+            SettingBoxPrintHeight.Text = Properties.Settings.Default.PrintHeight.ToString();
+            SettingBoxMoveHeight.Text = Properties.Settings.Default.MoveHeight.ToString();
+            SettingBoxPrintSpeed.Text = Properties.Settings.Default.PrintSpeed.ToString();
+            SettingBoxMoveSpeed.Text = Properties.Settings.Default.MoveSpeed.ToString();
         }
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
@@ -32,42 +40,42 @@ namespace SVGtoGCODE
 
         private void SettingBoxSizeX_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            Properties.Settings.Default.SizeX = int.Parse(SettingBoxSizeX.Text);
         }
 
         private void SettingBoxSizeY_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            Properties.Settings.Default.SizeY = int.Parse(SettingBoxSizeY.Text);
         }
 
         private void SettingBoxOffsetX_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            Properties.Settings.Default.OffsetX = int.Parse(SettingBoxOffsetX.Text);
         }
 
         private void SettingBoxOffsetY_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            Properties.Settings.Default.OffsetY = int.Parse(SettingBoxOffsetY.Text);
         }
 
         private void SettingBoxPrintHeight_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            Properties.Settings.Default.PrintHeight = int.Parse(SettingBoxPrintHeight.Text);
         }
 
         private void SettingBoxMoveHeight_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            Properties.Settings.Default.MoveHeight = int.Parse(SettingBoxMoveHeight.Text);
         }
 
         private void SettingBoxPrintSpeed_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            Properties.Settings.Default.PrintSpeed = int.Parse(SettingBoxPrintSpeed.Text);
         }
 
         private void SettingBoxMoveSpeed_TextChanged(object sender, TextChangedEventArgs e)
         {
-
+            Properties.Settings.Default.MoveSpeed = int.Parse(SettingBoxMoveSpeed.Text);
         }
     }
 }
