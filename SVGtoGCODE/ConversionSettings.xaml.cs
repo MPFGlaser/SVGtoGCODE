f74361d83583bee4aaac643aea7f835448c0a793
Max Glaser <mpf.glaser@gmail.com>
2020-01-14 11:38:44 +0100

    Added button to reset settings to default values.

--- SVGtoGCODE/ConversionSettings.xaml.cs
+++ SVGtoGCODE/ConversionSettings.xaml.cs
@@ -23,6 +23,11 @@ namespace SVGtoGCODE
         public ConversionSettings()
         {
             InitializeComponent();
+            RetrieveSettings();
+        }
+
+        private void RetrieveSettings()
+        {
             SettingBoxSizeX.Text = Properties.Settings.Default.SizeX.ToString();
             SettingBoxSizeY.Text = Properties.Settings.Default.SizeY.ToString();
             SettingBoxOffsetX.Text = Properties.Settings.Default.OffsetX.ToString();
@@ -32,50 +37,93 @@ namespace SVGtoGCODE
             SettingBoxPrintSpeed.Text = Properties.Settings.Default.PrintSpeed.ToString();
             SettingBoxMoveSpeed.Text = Properties.Settings.Default.MoveSpeed.ToString();
         }
+
+        private void DefaultSettings()
+        {
+            Properties.Settings.Default.SizeX = 148;
+            Properties.Settings.Default.SizeY = 105;
+            Properties.Settings.Default.OffsetX = -74;
+            Properties.Settings.Default.OffsetY = 70;
+            Properties.Settings.Default.PrintHeight = -50;
+            Properties.Settings.Default.MoveHeight = -30;
+            Properties.Settings.Default.PrintSpeed = 15;
+            Properties.Settings.Default.MoveSpeed = 50;
+            RetrieveSettings();
+        }
+
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
-            Regex regex = new Regex("[^0-9]+");
+            Regex regex = new Regex("[^0-9-]+");
             e.Handled = regex.IsMatch(e.Text);
         }
 
         private void SettingBoxSizeX_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Properties.Settings.Default.SizeX = int.Parse(SettingBoxSizeX.Text);
+            if (SettingBoxSizeX.Text.Length > 0)
+            {
+                Properties.Settings.Default.SizeX = int.Parse(SettingBoxSizeX.Text);
+            }
         }
 
         private void SettingBoxSizeY_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Properties.Settings.Default.SizeY = int.Parse(SettingBoxSizeY.Text);
+            if (SettingBoxSizeY.Text.Length > 0)
+            {
+                Properties.Settings.Default.SizeY = int.Parse(SettingBoxSizeY.Text);
+            }
         }
 
         private void SettingBoxOffsetX_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Properties.Settings.Default.OffsetX = int.Parse(SettingBoxOffsetX.Text);
+            if (SettingBoxOffsetX.Text.Length > 0)
+            {
+                Properties.Settings.Default.OffsetX = int.Parse(SettingBoxOffsetX.Text);
+            }
         }
 
         private void SettingBoxOffsetY_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Properties.Settings.Default.OffsetY = int.Parse(SettingBoxOffsetY.Text);
+            if (SettingBoxOffsetY.Text.Length > 0)
+            {
+                Properties.Settings.Default.OffsetY = int.Parse(SettingBoxOffsetY.Text);
+            }
         }
 
         private void SettingBoxPrintHeight_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Properties.Settings.Default.PrintHeight = int.Parse(SettingBoxPrintHeight.Text);
+            if (SettingBoxPrintHeight.Text.Length > 0)
+            {
+                Properties.Settings.Default.PrintHeight = int.Parse(SettingBoxPrintHeight.Text);
+            }
         }
 
         private void SettingBoxMoveHeight_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Properties.Settings.Default.MoveHeight = int.Parse(SettingBoxMoveHeight.Text);
+            if (SettingBoxMoveHeight.Text.Length > 0)
+            {
+                Properties.Settings.Default.MoveHeight = int.Parse(SettingBoxMoveHeight.Text);
+            }
         }
 
         private void SettingBoxPrintSpeed_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Properties.Settings.Default.PrintSpeed = int.Parse(SettingBoxPrintSpeed.Text);
+            if (SettingBoxPrintSpeed.Text.Length > 0)
+            {
+                Properties.Settings.Default.PrintSpeed = int.Parse(SettingBoxPrintSpeed.Text);
+            }
         }
 
         private void SettingBoxMoveSpeed_TextChanged(object sender, TextChangedEventArgs e)
         {
-            Properties.Settings.Default.MoveSpeed = int.Parse(SettingBoxMoveSpeed.Text);
+            if (SettingBoxMoveSpeed.Text.Length > 0)
+            {
+                Properties.Settings.Default.MoveSpeed = int.Parse(SettingBoxMoveSpeed.Text);
+            }
+        }
+
+        private void ResetButton_Click(object sender, RoutedEventArgs e)
+        {
+            DefaultSettings();
         }
     }
 }
