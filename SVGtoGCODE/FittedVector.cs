2f26c1f95e058841e4592e23b209027da390d9df
Max Glaser <mpf.glaser@gmail.com>
2020-01-14 14:28:42 +0100

    Added scaling multiplier Added rounding down to two decimals for scaling multiplier

--- SVGtoGCODE/FittedVector.cs
+++ SVGtoGCODE/FittedVector.cs
@@ -11,9 +11,18 @@ namespace SVGtoGCODE
     // inherit from vector
     public class FittedVector : Vector
     {
+        private int originalHeight;
+        private int originalWidth;
+        private int fittedHeight;
+        private int fittedWidth;
+        private double scalingMultiplier;
+
         // Should hold the info for the instance of Vector which has been fitted in a specific printing/work space
         public FittedVector(XmlDocument vector)
         {
+            Workspace workspace = new Workspace();
+            fittedHeight = workspace.sizeY();
+            fittedWidth = workspace.sizeX();
             GetDocumentSize(vector);
         }
 
@@ -21,12 +30,19 @@ namespace SVGtoGCODE
         {
             // This works!
             // https://stackoverflow.com/questions/933687/read-xml-attribute-using-xmldocument
+
+            // Reads <svg> tag from xml file, then saves the height and width attributes.
             XmlNodeList elemList = vector.GetElementsByTagName("svg");
             for (int i = 0; i < elemList.Count; i++)
             {
-                string attrVal = elemList[i].Attributes["height"].Value;
-                string height = attrVal;
+                originalHeight = int.Parse(elemList[i].Attributes["height"].Value);
+                originalWidth = int.Parse(elemList[i].Attributes["width"].Value);
             }
+
+            // Calculates scaling multiplier
+            scalingMultiplier = (double)fittedHeight / (double)originalHeight;
+            scalingMultiplier = Math.Floor(scalingMultiplier * 100d) / 100d;
+
         }
     }
 }
