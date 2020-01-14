da065d010cce056ac282c6571134fb6069c507fd
Max Glaser <mpf.glaser@gmail.com>
2020-01-14 16:52:25 +0100

    Added Constructor, Add, and Save methods to GCode class.

--- SVGtoGCODE/GCode.cs
+++ SVGtoGCODE/GCode.cs
@@ -8,7 +8,28 @@ namespace SVGtoGCODE
 {
     public class GCode
     {
+        List<string> GCodeCommands;
         // Is able to convert instance of FittedVector into GCode that complies with the restrictions given in instance of Workspace.
         // Should be able to generate, save, and export GCode.
+        public GCode()
+        {
+            GCodeCommands = new List<string>();
+        }
+
+        private void AddCommand(int x, int y, int z)
+        {
+            GCodeCommands.Add("G1" + " X" + x.ToString() + " Y" + y.ToString() + " Z" + z.ToString());
+        }
+
+        private void AddCommand(int x, int y, int z, int f)
+        {
+            GCodeCommands.Add("G1" + " X" + x.ToString() + " Y" + y.ToString() + " Z" + z.ToString() + " F" + f.ToString());
+        }
+
+        public void SaveGCode()
+        {
+            // Should write list of commands to txt file.
+            // https://stackoverflow.com/questions/15300572/saving-lists-to-txt-file
+        }
     }
 }
